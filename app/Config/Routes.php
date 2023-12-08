9e97361fc960a358623337e1667290363950813a
Yuki <sobrouesselogin@hotmail.com>
2023-12-07 22:34:08 -0300

    adicionado filtro de autenticação no endpoint de logout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,8 +12,8 @@ $routes->get('/logged-out', [AuthController::class, 'loggedOut']);
 $routes->post('/user_login', [AuthController::class, 'userLogin']);
 $routes->get('/users', [ApiController::class, 'getUsers'], ['filter'=>'checkApiAuth']);
 
-
 // service('auth')->routes($routes);
 //sobrescreve logout padrão do shield (apenas para adicionar filtro de autenticação)
+//solução baseada na documentação https://codeigniter4.github.io/shield/customization/route_config/
 service('auth')->routes($routes, ['except' => ['logout']]);
 $routes->get('logout', [AuthController::class, 'userLogout'], ['filter'=>'checkApiAuth']);
